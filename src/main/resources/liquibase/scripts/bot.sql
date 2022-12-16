79e83cd8cf6fbf1ce74d9316be397c7256cde504
Anastasiyu <asy.safiulina@mail.ru>
2022-12-16 14:52:03 +0300

    SQL +

--- src/main/resources/liquibase/scripts/bot.sql
+++ src/main/resources/liquibase/scripts/bot.sql
@@ -5,14 +5,23 @@ CREATE TABLE Notification_task
 (
     id   SERIAL,
     task TEXT,                       -- планы
-    currentDate LocalDateTime,
+    currentDate TIMESTAMP);
+CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE notification_task
+(
+    id             BIGINT NOT NULL,
+    task           VARCHAR(255),
+    "current_date" TIMESTAMP WITHOUT TIME ZONE,
+    user_id        BIGINT,
+    CONSTRAINT pk_notification_task PRIMARY KEY (id)
+);
 
     -- changeset safiulina:2 добавление таблицы
-    CREATE TABLE usersDataTable (
+    CREATE TABLE "User" (
         id int4 primary key,         -- id
         name varchar (100) NOT NULL, -- имя пользователя
         registeredAt Timestamp
         );
 
 
-)
